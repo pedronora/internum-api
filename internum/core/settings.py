dac414a51e2e5af80e4f604595f6968d38baad37
Pedro Nora <pedro@nora.vc>
2025-08-11 10:22:40 -0300

    feat: Set up infrastructure for PostgreSQL database integration in the project

---
+++ internum/core/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env.development', env_file_encoding='utf-8'
+    )
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_PASSWORD: str
+    DATABASE_URL: str = ''
+
+    def __init__(self, **values):
+        super().__init__(**values)
+        self.DATABASE_URL = f'postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
