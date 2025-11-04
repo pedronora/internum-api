fc88ad8fb63bddca8f95bbc95571272ab7fce15a
Pedro Nora <pedro@nora.vc>
2025-11-03 21:25:41 -0300

    feat: added create admin script

---
+++ internum/scripts/seed_admin.py
@@ -0,0 +1,45 @@
+import asyncio
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from internum.core.database import get_session
+from internum.core.security import get_password_hash
+from internum.core.settings import Settings
+from internum.modules.users.enums import Role, Setor
+from internum.modules.users.models import User
+
+settings = Settings()
+Session = Annotated[AsyncSession, Depends(get_session)]
+
+
+async def create_admin(session: Session):
+    admin = User(
+        name=settings.ADMIN_NAME,
+        username=settings.ADMIN_USERNAME,
+        email=settings.ADMIN_EMAIL,
+        password=get_password_hash(settings.ADMIN_PASSWORD),
+        role=Role.ADMIN,
+        setor=Setor.ADMINISTRATIVO,
+        subsetor='Apoio',
+    )
+
+    session.add(admin)
+    await session.commit()
+
+
+if __name__ == '__main__':
+
+    async def main():
+        session_generator = get_session()
+        try:
+            session = await anext(session_generator)
+            await create_admin(session)
+        finally:
+            try:
+                await anext(session_generator)
+            except StopAsyncIteration:
+                pass
+
+    asyncio.run(main())
