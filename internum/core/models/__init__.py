3231e10f8a55eaf0886a0949626052a6b167bb0e
Pedro Nora <pedro@nora.vc>
2026-08-03 19:22:51 -0300

    feat(vacation): integrate vacation module into API
    - Register vacation router in main API router (/api/v1/vacation)
    - Import vacation models in core models registry for SQLAlchemy metadata

--- internum/core/models/__init__.py
+++ internum/core/models/__init__.py
@@ -3,3 +3,4 @@ from internum.modules.legal_briefs import models as legal_briefs_model
 from internum.modules.library import models as library_model
 from internum.modules.notices import models as notices_model
 from internum.modules.users import models as users_model
+from internum.modules.vacation import models as vacation_model
